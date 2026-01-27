0d1b349cac5eabdad0cc1ef5d44cd8249bdd649a
Emmett J Bicknell <165820096+boydoiloveturtles@users.noreply.github.com>
2026-01-26 21:45:49 -0500

    Second quicksort done

--- Project 1/sortcomparison/MiddleQuickSort.java
+++ Project 1/sortcomparison/MiddleQuickSort.java
@@ -1,7 +1,46 @@
 package sortcomparison;
 
 public class MiddleQuickSort {
-    static void Sort(int[] array){
-        
+    static void Quicksort(int[] array, int first, int last){
+        if(first < last){
+            int mid = Partition(array, first, last);
+            Quicksort(array, first, mid-1);
+            Quicksort(array, mid+1, last);
+        }
+    }
+    private static void swap(int[] array, int a, int b){
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
+    private static int Partition(int[] array, int first, int last){
+    
+        int lower = first;
+        int upper = last - 1;
+
+        //this swap places the middle element at the pivot position
+        swap(array, last, ((first + last) / 2));
+        int pivot = array[last];
+
+        while(lower < upper){
+            while(lower <= upper && array[upper] >= pivot){
+                upper--;
+            }
+            while(lower <= upper && array[lower] <= pivot){
+                lower++;
+            }
+            if(lower < upper){
+                swap(array, lower, upper);
+            }
+        }
+        swap(array, lower, last);
+        return lower;
+    }
+    public static void main(String[] args){
+        int[] a = {0,4,5,6,8,7,4,2};
+        Quicksort(a,0,a.length-1);
+        for(int i = 0; i < a.length; i++){
+            System.out.println(a[i]);
+        }
     }
 }
