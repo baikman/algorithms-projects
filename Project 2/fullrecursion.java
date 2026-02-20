3c4964858789efc1dac31d99728731fa51d2823c
Emmett J Bicknell <165820096+boydoiloveturtles@users.noreply.github.com>
2026-02-20 14:20:00 -0500

    Added NumCoins method; it works

--- Project 2/fullrecursion.java
+++ Project 2/fullrecursion.java
@@ -1,3 +1,26 @@
 public class fullrecursion{
-
+    static int numDenom = 4;
+    static int[] Denom = {1,7,17,37};
+    static int NumCoins(Integer n){
+        if(n == 0){
+            return 0;
+        }
+        int bestNum = (int) Double.POSITIVE_INFINITY;
+        Integer bestK = 0;
+        for(int k = 0; k < numDenom; k++){
+            Integer newVal = n - Denom[k];
+            if(newVal < 0){
+                break;
+            }
+            Integer newCoins = NumCoins(newVal) + 1;
+            if(newCoins < bestNum){
+                bestNum = newCoins;
+                bestK = k;
+            }
+        }
+        return bestNum;
+    }
+    public static void main(String[] args) {
+        System.out.println(NumCoins(22));
+    }
 }
