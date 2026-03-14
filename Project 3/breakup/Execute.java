96a9f9bb37b49b1996337abf6650af2f1d05a962
Emmett J Bicknell <165820096+boydoiloveturtles@users.noreply.github.com>
2026-03-14 16:13:42 -0400

    I think I finished Node?

---
+++ Project 3/breakup/Execute.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Execute {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        Node.count = 0;
+        int numProblems = scan.nextInt();
+        for(int i = 0; i < numProblems; i++){
+            int numMonths = 0;
+            int n = scan.nextInt();
+            int m = scan.nextInt();
+            int k = scan.nextInt();
+            int l = scan.nextInt();
+            Node[][][] array = new Node[n][m][l];
+            for(int j = 0; j < l; j++){
+                
+            }
+        }
+        
+        
+    }
+}
