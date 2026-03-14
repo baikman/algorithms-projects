0129da98ac450e945082f5cd2454756eb931ae73
Emmett J Bicknell <165820096+boydoiloveturtles@users.noreply.github.com>
2026-03-14 13:53:15 -0400

    Consolidating into Node.java

--- Project 3/breakup/Node.java
+++ Project 3/breakup/Node.java
@@ -1,15 +1,43 @@
 public class Node {
     int key;
+    int rank;
     Node parent;
     public Node(int i, Node p){
         key=i;
         parent = p;
+        rank = 0;
     }
     public Node(int i){
         this(i,null);
         this.parent = this;
     }
     public Node(){
-        
+
+    }
+    void setParent(Node a){
+        this.parent = a;
+    }
+    Node findSet(Node current){
+        if(current.parent != current){
+            current.parent = findSet(current.parent);
+        }
+        return current.parent;
+    }
+    void unionSets(Node a, Node b){
+        if (a.rank > b.rank){
+            (b).setParent(a);
+        }
+        else{
+            if(a.rank == b.rank){
+                b.rank ++;
+            }
+            a.setParent(b);
+        }
+    }
+    Node makeSet(int i){
+        return new Node(i);
+
+
+        //we then need to check for unions
     }
 }
