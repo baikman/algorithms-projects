0002fc0a384d0a70dbd8209a394ec3faaed21974
Emmett J Bicknell <165820096+boydoiloveturtles@users.noreply.github.com>
2026-03-17 16:09:38 -0400

    Getting really close

--- Project 3/breakup/Execute.java
+++ Project 3/breakup/Execute.java
@@ -1,5 +1,3 @@
-import java.util.List;
-import java.util.Scanner;
 import java.util.*;
 
 public class Execute {
@@ -13,18 +11,36 @@ public class Execute {
             int k = scan.nextInt();
             int l = scan.nextInt();
             Node[] array = new Node[n*m*k];
+            Stack<int[]> dominionStack = new Stack<int[]>();
             for(int i = 0; i < l; i++){
                 int numDominions = scan.nextInt();
+                int[] storedDominions = new int[numDominions];
                 for(int j = 0; j < numDominions; j++){
-                    int newKey = scan.nextInt();
-                    Node newNode = Node.makeSet(newKey);
-                    array[newKey] = newNode;
+                    storedDominions[j] = scan.nextInt();
+                }
+                dominionStack.push(storedDominions);
+            }
+            while(!dominionStack.isEmpty()){
+                int[] dominionArray = dominionStack.pop();
+                for(int val: dominionArray){
+                    Node newNode = Node.makeSet(val);
+                    array[val] = newNode;
                     Node.checkAdjacencies(newNode, n, m, k, array);
                 }
                 if(Node.count > 1){
                         numMonths++;
                 }
             }
+
+
+
+            
+                //FIXME We'll use this
+                    //int newKey = scan.nextInt();
+                    //Node newNode = Node.makeSet(newKey);
+                    //array[newKey] = newNode;
+                    //Node.checkAdjacencies(newNode, n, m, k, array);
+            //This shows the number of months things were not connected.
             System.out.println(numMonths);
             
             
