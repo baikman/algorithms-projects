85fa5f0a61f677e6f2207f1f2763f567f8eed011
Emmett J Bicknell <165820096+boydoiloveturtles@users.noreply.github.com>
2026-03-30 14:12:17 -0400

    Adding more

---
+++ Project 4/Puzzle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Puzzle {
+    Container[][] array = new Container[6][6];
+    Boolean testState(String currstate){
+        //test to see if red car is at the exit
+        return true;
+        //FIXME needs to be adjusted
+    }
+    public static void main(String[] args){
+        //input
+        Scanner scan = new Scanner(System.in);
+        Integer numCars = scan.nextInt();
+
+    }
+}
